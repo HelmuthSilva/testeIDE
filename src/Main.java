780510159521f437ab9f3d16d6228963e7b45e13
HelmuthSilva <helmuthmartines@hotmail.com>
2024-10-21 08:45:05 -0300

    Mostrando a mensagem inicial

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //SouRCe -> Fonte
+        System.out.println("Iniciando e testando!");
     }
 }
